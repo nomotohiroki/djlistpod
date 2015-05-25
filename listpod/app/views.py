91e911b2ec0534b95597f486d475919ab3120def
nomotohiroki <musictripper@gmail.com>
2015-05-25 23:16:56 +0900

    improve dashboard and add html view

--- listpod/app/views.py
+++ listpod/app/views.py
@@ -13,8 +13,8 @@ from app.client           import Youtube
 
 
 # global instanses
-ydl = YoutubeDL()
-ie  = YoutubeIE(ydl)
+ydl = None
+ie  = None
 y   = None
 
 
@@ -28,54 +28,60 @@ def index(request):
     playlist_info     = None
     subscription_info = None
     channel_info      = None
-    if y != None and y.is_authorized():
+    if y.is_authorized():
         playlist_info     = y.playlists()
         subscription_info = y.subscriptions()
         channel_info      = y.channel()
     
-    return render_to_response('dashboard.html', { 'channel': channel_info, 'playlists': playlist_info, 'subscriptions': subscription_info }, context_instance=RequestContext(request))
+    return render_to_response(
+        'dashboard.html',
+        {
+            'authorized':    y.is_authorized(),
+            'channel':       channel_info,
+            'playlists':     playlist_info,
+            'subscriptions': subscription_info,
+        },
+        context_instance=RequestContext(request)
+    )
 
 def playlist(request, playlist_id):
     _init_ycl(request)
-    if y == None or not y.is_authorized():
-        return HttpResponse(u'Not Authorized.')
     playlist = y.playlist(playlist_id)
     return _render_podcast(request, playlist)
 
 def favorites(request, user_name=None):
     _init_ycl(request)
-    if y == None or not y.is_authorized():
-        return HttpResponse(u'Not Authorized.')
     playlist = y.favorites(user_name)
     return _render_podcast(request, playlist)
 
 def video(request, video_id):
     _init_ycl(request)
-    if y == None or not y.is_authorized():
-        return HttpResponse(u'Not Authorized.')
-    o = ie.extract('https://www.youtube.com/watch?v=' + video_id)
+    o = ie.extract(video_id)
     selected_url = ydl.select_format('best', o['formats'])['url']
     return redirect(selected_url)
 
 def oauth2callback(request):
     y.exchange(request.GET['code'])
-    return redirect("/favorites/")
+    return redirect("/")
 
 
 def _init_ycl(request):
-    global y
+    global ydl, ie, y
     if y == None:
+        ydl = YoutubeDL()
+        ie  = YoutubeIE(ydl)
         y = Youtube(request.scheme + "://" + request.get_host() + "/oauth2callback")
 
 
 def _render_podcast(request, playlist):
-    response = render_to_response('podcast.rss',
+    mode = request.GET['mode'] if 'mode' in request.GET else None
+    response = render_to_response('feed.html' if mode == 'html' else 'podcast.rss',
                               {
                                   'request': request,
-                                  'title':  playlist['title'],
-                                  'videos': playlist['videos'],
+                                  'title':  playlist['title']  if playlist else None,
+                                  'videos': playlist['videos'] if playlist else None,
                               },
-                              content_type = 'application/rss+xml',
+                              content_type = 'text/html' if mode == 'html' else 'application/rss+xml',
                               context_instance=RequestContext(request))
     response['Content-Length'] = len(response.content)
     return response
